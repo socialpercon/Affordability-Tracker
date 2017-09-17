90ed0fc5ea701a69ea0fbf93d4fabf61c683390f
Matthew Weser <matthew.weser@connexta.com>
2017-09-17 14:49:11 -0700

    Add other activities and make side navbar universal

--- AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/utils/Utils.java
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/utils/Utils.java
@@ -2,6 +2,14 @@ package com.mweser.affordabilitytracker.utils;
 
 import com.mweser.affordabilitytracker.R;
 import com.mweser.affordabilitytracker.activities.BankAccountActivity;
+import com.mweser.affordabilitytracker.activities.CreditPointsActivity;
+import com.mweser.affordabilitytracker.activities.ExpensesActivity;
+import com.mweser.affordabilitytracker.activities.ProjectionActivity;
+import com.mweser.affordabilitytracker.activities.ProjectionListActivity;
+import com.mweser.affordabilitytracker.activities.SettingsActivity;
+import com.mweser.affordabilitytracker.activities.ThresholdsActivity;
+import com.mweser.affordabilitytracker.activities.WishlistActivity;
+import com.mweser.affordabilitytracker.activities.WishlistScheduleActivity;
 
 import android.app.Activity;
 import android.content.Context;
@@ -22,27 +30,35 @@ public class Utils
 
         } else if (id == R.id.nav_expenses) {
             newToast(appContext, "This is the expenses page");
+            Utils.startActivity(baseContext, activity, ExpensesActivity.class);
 
         } else if (id == R.id.nav_thresholds) {
             newToast(appContext, "This is the thresholds page");
+            Utils.startActivity(baseContext, activity, ThresholdsActivity.class);
 
         } else if (id == R.id.nav_wishlist) {
             newToast(appContext, "This is the wishlist page");
+            Utils.startActivity(baseContext, activity, WishlistActivity.class);
 
         } else if (id == R.id.nav_projection) {
             newToast(appContext, "This is the projection page");
+            Utils.startActivity(baseContext, activity, ProjectionActivity.class);
 
         } else if (id == R.id.nav_future_events_list) {
             newToast(appContext, "This is the future events page");
+            Utils.startActivity(baseContext, activity, ProjectionListActivity.class);
 
         } else if (id == R.id.nav_wishlist_schedule) {
             newToast(appContext, "This is the wishlist schedule page");
+            Utils.startActivity(baseContext, activity, WishlistScheduleActivity.class);
 
         } else if (id == R.id.nav_credit_points) {
             newToast(appContext, "This is the credit card points page");
+            Utils.startActivity(baseContext, activity, CreditPointsActivity.class);
 
         } else if (id == R.id.nav_prefs) {
             newToast(appContext, "This is the preferences page");
+            Utils.startActivity(baseContext, activity, SettingsActivity.class);
 
         }
 
@@ -56,7 +72,7 @@ public class Utils
 
     public static void startActivity(Context baseContext, Activity currentActivity, Class<?> nextActivityClass)
     {
-        currentActivity.finish();
+//        currentActivity.finish();
         currentActivity.startActivity(new Intent(baseContext, nextActivityClass));
     }
 
