37e9582d0752e8abd60b29dfc376710b65418e7f
Matthew Weser <matthew.weser@connexta.com>
2017-09-17 13:31:48 -0700

    Clean up navbar functionality with new activity

--- AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/activities/BankAccountActivity.java
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/activities/BankAccountActivity.java
@@ -1,10 +1,12 @@
-package com.mweser.affordabilitytracker;
+package com.mweser.affordabilitytracker.activities;
+
+import com.mweser.affordabilitytracker.R;
+import com.mweser.affordabilitytracker.utils.Utils;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,21 +14,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class BankAccountActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_bank_account_management);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        fab.setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null)
                         .show();
@@ -47,31 +54,37 @@ public class MainActivity extends AppCompatActivity
     }
 
     @Override
-    public void onBackPressed() {
+    public void onBackPressed()
+    {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+        {
             drawer.closeDrawer(GravityCompat.START);
-        } else {
+        } else
+        {
             super.onBackPressed();
         }
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.bank_account_management, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
@@ -80,23 +93,9 @@ public class MainActivity extends AppCompatActivity
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
+    public boolean onNavigationItemSelected(MenuItem item)
+    {
+        Utils.navBarSwitch(getApplicationContext(), getBaseContext(), this, item);
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/utils/Utils.java
@@ -0,0 +1,63 @@
+package com.mweser.affordabilitytracker.utils;
+
+import com.mweser.affordabilitytracker.R;
+import com.mweser.affordabilitytracker.activities.BankAccountActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class Utils
+{
+    public static void navBarSwitch(Context appContext, Context baseContext, Activity activity, MenuItem item)
+    {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_accounts) {
+
+            newToast(appContext, "This is the accounts page");
+            Utils.startActivity(baseContext, activity, BankAccountActivity.class);
+
+        } else if (id == R.id.nav_expenses) {
+            newToast(appContext, "This is the expenses page");
+
+        } else if (id == R.id.nav_thresholds) {
+            newToast(appContext, "This is the thresholds page");
+
+        } else if (id == R.id.nav_wishlist) {
+            newToast(appContext, "This is the wishlist page");
+
+        } else if (id == R.id.nav_projection) {
+            newToast(appContext, "This is the projection page");
+
+        } else if (id == R.id.nav_future_events_list) {
+            newToast(appContext, "This is the future events page");
+
+        } else if (id == R.id.nav_wishlist_schedule) {
+            newToast(appContext, "This is the wishlist schedule page");
+
+        } else if (id == R.id.nav_credit_points) {
+            newToast(appContext, "This is the credit card points page");
+
+        } else if (id == R.id.nav_prefs) {
+            newToast(appContext, "This is the preferences page");
+
+        }
+
+    }
+    
+    public static void newToast(Context appContext, String message)
+    {
+        Toast.makeText(appContext, message, Toast.LENGTH_LONG)
+                .show();
+    }
+
+    public static void startActivity(Context baseContext, Activity currentActivity, Class<?> nextActivityClass)
+    {
+        currentActivity.finish();
+        currentActivity.startActivity(new Intent(baseContext, nextActivityClass));
+    }
+
+}
