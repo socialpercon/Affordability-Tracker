3f0db67a99c7f6a9e07b155f5d4731dda987c652
Matthew Weser <matthew.weser@connexta.com>
2017-09-17 18:42:17 -0700

    Start work on create new bank account activity

--- AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/controller/ActivityUtils.java
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/controller/ActivityUtils.java
@@ -24,40 +24,30 @@ public class ActivityUtils
         int id = item.getItemId();
 
         if (id == R.id.nav_accounts) {
-
-            newToast(appContext, "This is the accounts page");
             ActivityUtils.startActivity(baseContext, activity, BankAccountActivity.class);
 
         } else if (id == R.id.nav_expenses) {
-            newToast(appContext, "This is the expenses page");
             ActivityUtils.startActivity(baseContext, activity, ExpensesActivity.class);
 
         } else if (id == R.id.nav_thresholds) {
-            newToast(appContext, "This is the thresholds page");
             ActivityUtils.startActivity(baseContext, activity, ThresholdsActivity.class);
 
         } else if (id == R.id.nav_wishlist) {
-            newToast(appContext, "This is the wishlist page");
             ActivityUtils.startActivity(baseContext, activity, WishlistActivity.class);
 
         } else if (id == R.id.nav_projection) {
-            newToast(appContext, "This is the projection page");
             ActivityUtils.startActivity(baseContext, activity, ProjectionActivity.class);
 
         } else if (id == R.id.nav_future_events_list) {
-            newToast(appContext, "This is the future events page");
             ActivityUtils.startActivity(baseContext, activity, ProjectionListActivity.class);
 
         } else if (id == R.id.nav_wishlist_schedule) {
-            newToast(appContext, "This is the wishlist schedule page");
             ActivityUtils.startActivity(baseContext, activity, WishlistScheduleActivity.class);
 
         } else if (id == R.id.nav_credit_points) {
-            newToast(appContext, "This is the credit card points page");
             ActivityUtils.startActivity(baseContext, activity, CreditPointsActivity.class);
 
         } else if (id == R.id.nav_prefs) {
-            newToast(appContext, "This is the preferences page");
             ActivityUtils.startActivity(baseContext, activity, SettingsActivity.class);
 
         }
