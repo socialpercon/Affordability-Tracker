b040500c68c107dce644a5f30264247ffca00ddd
Matthew Weser <matthew.weser@connexta.com>
2017-09-18 07:33:23 -0700

    Add BankAccountManager file to project

---
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/controller/BankAccountManager.java
@@ -0,0 +1,14 @@
+package com.mweser.affordabilitytracker.controller;
+
+public class BankAccountManager
+{
+    public static String getBankAccountListing()
+    {
+        String bankAccountListing = "";
+
+
+
+        return bankAccountListing;
+    }
+
+}
