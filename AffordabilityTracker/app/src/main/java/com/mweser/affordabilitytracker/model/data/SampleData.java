3bb651d2eabfd4799ed59e0c4f003d7424fffe37
Matthew Weser <matthew.weser@connexta.com>
2017-09-19 11:31:23 -0700

    Add sample data inputs

---
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/model/data/SampleData.java
@@ -0,0 +1,34 @@
+package com.mweser.affordabilitytracker.model.data;
+
+public class SampleData
+{
+
+
+
+
+    public void injectSampleBankAccountData()
+    {
+
+
+    }
+
+    public void injectSampleRecurEventsData()
+    {
+
+
+    }
+
+    public void injectSampleThresholdsData()
+    {
+
+
+    }
+
+    public void injectSampleWishlistData()
+    {
+
+
+    }
+
+
+}
