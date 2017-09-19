8a9b54b00fb0327530d142cad04054e59c2eac1e
Matthew Weser <matthew.weser@connexta.com>
2017-09-18 18:02:14 -0700

    Update logo and main splash screen

--- AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/view/MainActivity.java
+++ AffordabilityTracker/app/src/main/java/com/mweser/affordabilitytracker/view/MainActivity.java
@@ -8,7 +8,6 @@ import com.mweser.affordabilitytracker.model.data.DatabaseOperations;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -89,9 +88,8 @@ public class MainActivity extends AppCompatActivity
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null)
-                        .show();
+                ActivityUtils.startActivity(getBaseContext(), MainActivity.this, CreateBankAccountActivity.class);
+
             }
         });
 
